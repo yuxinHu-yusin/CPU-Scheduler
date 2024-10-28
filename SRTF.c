1a1cfdbb97445c3bade5c1d711a3bfeebae18ac1
DannyLLL <lai.yihs@northeastern.com>
2024-10-28 00:54:41 -0700

    Get rid of the extra while loop

--- SRTF.c
+++ SRTF.c
@@ -26,7 +26,7 @@ int main(int argc, char* argv[]) {
     queue_t* finished_queue = create_queue();   // put finished job here
 
     if (test_mode){
-        load_from_file("job2.txt", job_queue);
+        load_from_file("job1.txt", job_queue);
     } else {
         // Load jobs from the file
         if (argc != 2) {
@@ -42,135 +42,134 @@ int main(int argc, char* argv[]) {
     clock++;
 
     // While look for mimic the cpu and clock time
-    while (!is_empty(job_queue) || !is_empty(wait_queue) || !is_empty(ready_queue)) {    
+    job_t* current_job = NULL;
 
-        job_t* current_job = NULL;
+    // loop of the process
+    while (!is_empty(job_queue) || !is_empty(wait_queue) || 
+            !is_empty(ready_queue) || current_job != NULL) {
 
-        // loop of the process
-        while (!is_empty(job_queue) || !is_empty(wait_queue) || 
-               !is_empty(ready_queue) || current_job != NULL) {
-
-            if (test_mode) {
-                printf("Time: %d\n", clock);
-                printf("job: %d  ready: %d  wait: %d\n",
-                        job_queue->count, ready_queue->count, wait_queue->count);
-            }
+        if (test_mode) {
+            printf("Time: %d\n", clock);
+            printf("job: %d  ready: %d  wait: %d\n",
+                    job_queue->count, ready_queue->count, wait_queue->count);
+        }
 
 
-            // check if any job in the arrive, 
-            //if so choose the one with shortest remaining time
-            if (check_arrive(job_queue, ready_queue, clock)) {
-                enqueue(ready_queue, current_job);
-                current_job = dequeue_short(ready_queue);
-
-            }
-
-
-            // check if any of the job in the wait queue has finish waiting
-            int origional_count = wait_queue->count;
-            for (int i = 0; i < origional_count; i++) {
-                job_t* temp = dequeue(wait_queue); // pull out a job from wait
-                temp->io_time++; // increment 1 to IO waiting time 
-                if (IO_complete()) {
-
-                    if (test_mode) {
-                        printf("IO Completed: %d\n", temp->pid);
-                    }
-
-                    enqueue(ready_queue, temp);
-                } else {
-                    enqueue(wait_queue, temp); // enqueue back if still need wait
-                }
-            }
-
-
-            // check if current job is null
-            // goes to the next clock cycle if so
-            if (current_job == NULL) {
-
-                // check if there is any job left in ready
-                current_job = dequeue_short(ready_queue);
-
-                // No job is ready, keep the clock ticking
-                if (current_job == NULL){
-                    clock++;
-                    continue;
-                }
-            }
-
-
-            // check if current job is completed
-            if (current_job->service_time == 0) {
-                current_job->end_time = clock;  // record the end time 
-                enqueue(finished_queue, current_job);  // put in finished_queue
-
-                if (test_mode) {
-                    printf("A job completed: ");
-                    print_job(current_job);
-                    printf("\n");
-                }
-
-                current_job = dequeue_short(ready_queue); //swap the job to another one
-
-
-                continue;
-            }
-            
-            
-
-
-
-            // set start time when first arrive
-            if (current_job->start_time == -1) {
-                current_job->start_time = clock;
-            }
-
-            // ckeck of the current job need IO
-            int state = IO_request();
-
-            if (test_mode) {
-                printf("Current Job: %d  Time Left: %d\n", current_job->pid, current_job->service_time);
-                printf("IO Request: %d\n", state);
-                printf("\n");
-            }
-
-
-            if (state == 1) {
-                enqueue(wait_queue, current_job);
-
-                
-                if (!is_empty(ready_queue)){
-                    // swap to the next job is there's more
-                    current_job = dequeue_short(ready_queue);
-                    continue;
-                } else {
-                    // just stay at current job and waiting
-                    current_job = NULL;
-                    continue;
-                }
-                
-            }
-
-
-            
-
-            // add 1 to ready_time to all the job in the ready queue
-            node_t* temp = ready_queue->front;
-            while (temp != NULL) {
-                job_t* to_add = (job_t*)temp->data;
-                to_add->ready_time++;
-                temp = temp->next;
-            }
-    
-            // increment clock time
-            clock++;
-            current_job->service_time--;
-
+        // check if any job in the arrive, 
+        //if so choose the one with shortest remaining time
+        if (check_arrive(job_queue, ready_queue, clock)) {
+            enqueue(ready_queue, current_job);
+            current_job = dequeue_short(ready_queue);
 
         }
 
 
+        // check if any of the job in the wait queue has finish waiting
+        int origional_count = wait_queue->count;
+        for (int i = 0; i < origional_count; i++) {
+            job_t* temp = dequeue(wait_queue); // pull out a job from wait
+            temp->io_time++; // increment 1 to IO waiting time 
+            if (IO_complete()) {
+
+                if (test_mode) {
+                    printf("IO Completed: %d\n", temp->pid);
+                }
+
+                enqueue(ready_queue, temp);
+            } else {
+                enqueue(wait_queue, temp); // enqueue back if still need wait
+            }
+        }
+
+
+        // check if current job is null
+        // goes to the next clock cycle if so
+        if (current_job == NULL) {
+
+            // check if there is any job left in ready
+            current_job = dequeue_short(ready_queue);
+
+            // No job is ready, keep the clock ticking
+            if (current_job == NULL){
+                clock++;
+                continue;
+            }
+        }
+
+
+        // check if current job is completed
+        if (current_job->service_time == 0) {
+            current_job->end_time = clock;  // record the end time 
+            enqueue(finished_queue, current_job);  // put in finished_queue
+
+            if (test_mode) {
+                printf("A job completed: ");
+                print_job(current_job);
+                printf("\n");
+            }
+
+            current_job = dequeue_short(ready_queue); //swap the job to another one
+
+
+            continue;
+        }
+        
+        
+
+
+
+        // set start time when first arrive
+        if (current_job->start_time == -1) {
+            current_job->start_time = clock;
+        }
+
+        // ckeck of the current job need IO
+        int state = IO_request();
+
+        if (test_mode) {
+            printf("Current Job: %d  Time Left: %d\n", current_job->pid, current_job->service_time);
+            printf("IO Request: %d\n", state);
+            printf("\n");
+        }
+
+
+        if (state == 1) {
+            enqueue(wait_queue, current_job);
+
+            
+            if (!is_empty(ready_queue)){
+                // swap to the next job is there's more
+                current_job = dequeue_short(ready_queue);
+                continue;
+            } else {
+                // just stay at current job and waiting
+                current_job = NULL;
+                continue;
+            }
+            
+        }
+
+
+        
+
+        // add 1 to ready_time to all the job in the ready queue
+        node_t* temp = ready_queue->front;
+        while (temp != NULL) {
+            job_t* to_add = (job_t*)temp->data;
+            to_add->ready_time++;
+            temp = temp->next;
+        }
+
+        // increment clock time
+        clock++;
+        current_job->service_time--;
+        current_job->cpu_time;
+
+
     }
+
+
+
 
     // need to print out everything for the finsiehd queue
     printf("        |  Total time       |  Total time       |  Total time   |\n");
@@ -182,9 +181,9 @@ int main(int argc, char* argv[]) {
     node_t* temp = finished_queue->front;
     int short_time = INT_MAX; //record the shortest job completion time
     int long_time = -1;       // record longest job completion time
-    int total_time_comp = 0;
-    int total_time_ready = 0;
-    int total_time_io = 0;
+    float total_time_comp = 0;
+    float total_time_ready = 0;
+    float total_time_io = 0;
 
 
     while (temp != NULL) {
@@ -222,9 +221,9 @@ int main(int argc, char* argv[]) {
     printf("Total number of jobs: %d\n", finished_queue->count);
     printf("Shortest job completion time: %d\n", short_time);
     printf("Longest job completion time: %d\n", long_time);
-    printf("Average job completion time: %d\n", total_time_comp / finished_queue->count);
-    printf("Average time in ready queue: %d\n", total_time_ready / finished_queue->count);
-    printf("Average time sleeping on I/O: %d\n", total_time_io / finished_queue->count);
+	printf("Average job completion time: %.3f\n", total_time_comp / finished_queue->count);
+	printf("Average time in ready queue: %.3f\n", total_time_ready / finished_queue->count);
+	printf("Average time sleeping on I/O: %.3f\n", total_time_io / finished_queue->count);
     printf("\n");
 
     
